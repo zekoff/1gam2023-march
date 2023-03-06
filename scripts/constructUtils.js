e0ebbd7d461648781a395780ff5f103d3b67faae
Michael Zekoff <zekoff@gmail.com>
2023-03-05 19:16:53 -0600

    Write Construct Array helper

---
+++ scripts/constructUtils.js
@@ -0,0 +1,51 @@
+// constructUtils.js
+
+/**
+ * Given a Construct 2D array, treat the first row as header info and unpack
+ * each data row into a dict.
+ * 
+ * The first column of each data row will be the key in an outer map.
+ * The value for each key will be a map (object). That object will have
+ * an attribute for each column in the header row, and that attribute's
+ * value will be the data row's contents. For any empty column, the value
+ * at that attribute will be an empty string.
+ * 
+ * Example Construct array:
+ * 0) Name   Cost    Power
+ * 1) Test   2       Low
+ * 2) Test2          5
+ * 
+ * Result object:
+ * {
+ *   "Test": {
+ *      "Cost": 2,
+ *      "Power": "Low"
+ *    },
+ *   "Test2": {
+ *      "Cost": "",
+ *      "Power": 5
+ *    }
+ * }
+ * 
+ * @param {*} array the Construct array to unpack
+ */
+export function unpackArrayToMapping(array) {
+  if (!array) return {};
+  if (array.width * array.height * array.depth === 0) return {};
+  if (array.height === 1) return {};
+  const headers = [];
+  for (let i = 0; i < array.width; i++) {
+    headers.push(String(array.getAt(i)));
+  }
+  const mapping = {};
+  for (let row = 1; row < array.height; row++) {
+    const data = {};
+    for (let col = 1; col < array.width; col++) {
+      let value = array.getAt(col, row);
+      if (value === "") value = null;
+      data[headers[col]] = value;
+    }
+    mapping[array.getAt(0, row)] = data;
+  }
+  return mapping;
+}
---
+++ scripts/importsForEvents.js
@@ -0,0 +1,4 @@
+// importsForEvents.js
+
+import * as FacilityUtils from "./facilityUtils.js";
+import * as ConstructUtils from "./constructUtils.js";
